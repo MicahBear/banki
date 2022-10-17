f9cbe58e4675677b1c9c239e3361191b7fbee3d0
MicahBear <micah.b.elm@gmail.com>
2022-10-17 14:56:28 -0700

    array

--- standup.js
+++ standup.js
@@ -36,3 +36,18 @@ fizzBuzz(15) //1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fi
 // domainName("http://www.zombie-bites.com") == "zombie-bites"
 // domainName("https://www.cnet.com") == "cnet"
 //  Use replace on the parts of the url you want gone.You can replace with an empty string ''.Then split the string using.as the seperator and take the first part[0]
+
+
+
+/**
+ * Oct 18
+ * Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+
+assert.strictEqual(sumMix([9, 3, '7', '3']), 22);
+assert.strictEqual(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42); 
+assert.strictEqual(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41); 
+
+ reduce the array adding each value to get a sum and make sure the current value is treated as a Number by using Number() 
+ */
