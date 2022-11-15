d4f81328dcd60ecd6223b889fc2c65410a38ceea
MicahBear <micah.b.elm@gmail.com>
2022-11-15 15:27:07 -0800

    filter,sort,map,shift

--- standup.js
+++ standup.js
@@ -171,3 +171,54 @@ function arrayDiff(a, b) {
 }
 //set is linear, .has is constant?
 // contains() for strings,  includes () for arrays, has() for objects is property for your set.
+
+/** 
+You need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+The input to the function will be an array of three distinct numbers.
+
+For example:
+
+gimme([2, 3, 1]) => 0
+2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+Another example(just to make sure it is clear):
+
+gimme([5, 10, 14]) => 1
+10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+Hint:
+spread array, sort it and grab the middle value.Throw that into indexOf the array itself - does time complexity matter for this problem ?
+*/
+
+function gimme(arr) {
+    // sort -> grab 1st index
+    // go back to original array 
+    // find indexOf num from above
+    //arr-> indexOf num^
+    return arr.indexOf([...arr].sort((a, b) => a - b)[1])
+}
+
+console.log(gimme([2, 3, 1], '0'))
+
+
+/**
+ * 
+ You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
+sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0]) 
+ */
+
+function sortArray(array) {
+    let oddList = array.filter(num => num % 2).sort((a, b) => b - a);
+
+    return array.map(num => {
+        return num % 2 ? oddList.pop() : num;
+    });
+}
